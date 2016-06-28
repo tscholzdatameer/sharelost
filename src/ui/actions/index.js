3c3608c4d1c3bf355e948da941e0eb733cc29a69
Jörg Weber <weber.joerg.h@gmail.com>
2016-06-28 11:01:34 +0200

    Add reducer and actions for "item" fetching

--- src/ui/actions/index.js
+++ src/ui/actions/index.js
@@ -1,138 +1,76 @@
-import { CALL_API, Schemas } from '../middleware/api'
+import spring from 'spring-data-rest-js';
 
-export const USER_REQUEST = 'USER_REQUEST'
-export const USER_SUCCESS = 'USER_SUCCESS'
-export const USER_FAILURE = 'USER_FAILURE'
+spring.requestConfig.baseURL = 'http://localhost:8083';
+const Item = spring.extend('items');
 
-// Fetches a single user from Github API.
-// Relies on the custom API middleware defined in ../middleware/api.js.
-function fetchUser(login) {
+export const REQUEST_ITEMS = 'REQUEST_ITEMS';
+export const RECEIVE_ITEMS = 'RECEIVE_ITEMS';
+
+function requestItems() {
   return {
-    [CALL_API]: {
-      types: [ USER_REQUEST, USER_SUCCESS, USER_FAILURE ],
-      endpoint: `users/${login}`,
-      schema: Schemas.USER
-    }
-  }
+    type: REQUEST_ITEMS
+  };
 }
 
-// Fetches a single user from Github API unless it is cached.
-// Relies on Redux Thunk middleware.
-export function loadUser(login, requiredFields = []) {
-  return (dispatch, getState) => {
-    const user = getState().entities.users[login]
-    if (user && requiredFields.every(key => user.hasOwnProperty(key))) {
-      return null
-    }
-
-    return dispatch(fetchUser(login))
-  }
-}
-
-export const REPO_REQUEST = 'REPO_REQUEST'
-export const REPO_SUCCESS = 'REPO_SUCCESS'
-export const REPO_FAILURE = 'REPO_FAILURE'
-
-// Fetches a single repository from Github API.
-// Relies on the custom API middleware defined in ../middleware/api.js.
-function fetchRepo(fullName) {
+function receiveItems(items) {
   return {
-    [CALL_API]: {
-      types: [ REPO_REQUEST, REPO_SUCCESS, REPO_FAILURE ],
-      endpoint: `repos/${fullName}`,
-      schema: Schemas.REPO
-    }
-  }
+    type: RECEIVE_ITEMS,
+    items
+  };
 }
 
-// Fetches a single repository from Github API unless it is cached.
-// Relies on Redux Thunk middleware.
-export function loadRepo(fullName, requiredFields = []) {
-  return (dispatch, getState) => {
-    const repo = getState().entities.repos[fullName]
-    if (repo && requiredFields.every(key => repo.hasOwnProperty(key))) {
-      return null
-    }
-
-    return dispatch(fetchRepo(fullName))
-  }
+export function fetchItems() {
+  return dispatch => {
+    dispatch(requestItems());
+    Item.findAll()
+      .then(items => dispatch(receiveItems(items)));
+  };
 }
 
-export const STARRED_REQUEST = 'STARRED_REQUEST'
-export const STARRED_SUCCESS = 'STARRED_SUCCESS'
-export const STARRED_FAILURE = 'STARRED_FAILURE'
+export const REQUEST_TOP_ITEMS = 'REQUEST_TOP_ITEMS';
+export const RECEIVE_TOP_ITEMS = 'RECEIVE_TOP_ITEMS';
 
-// Fetches a page of starred repos by a particular user.
-// Relies on the custom API middleware defined in ../middleware/api.js.
-function fetchStarred(login, nextPageUrl) {
+function requestTopItems() {
   return {
-    login,
-    [CALL_API]: {
-      types: [ STARRED_REQUEST, STARRED_SUCCESS, STARRED_FAILURE ],
-      endpoint: nextPageUrl,
-      schema: Schemas.REPO_ARRAY
-    }
-  }
+    type: REQUEST_TOP_ITEMS
+  };
 }
 
-// Fetches a page of starred repos by a particular user.
-// Bails out if page is cached and user didn’t specifically request next page.
-// Relies on Redux Thunk middleware.
-export function loadStarred(login, nextPage) {
-  return (dispatch, getState) => {
-    const {
-      nextPageUrl = `users/${login}/starred`,
-      pageCount = 0
-    } = getState().pagination.starredByUser[login] || {}
-
-    if (pageCount > 0 && !nextPage) {
-      return null
-    }
-
-    return dispatch(fetchStarred(login, nextPageUrl))
-  }
-}
-
-export const STARGAZERS_REQUEST = 'STARGAZERS_REQUEST'
-export const STARGAZERS_SUCCESS = 'STARGAZERS_SUCCESS'
-export const STARGAZERS_FAILURE = 'STARGAZERS_FAILURE'
-
-// Fetches a page of stargazers for a particular repo.
-// Relies on the custom API middleware defined in ../middleware/api.js.
-function fetchStargazers(fullName, nextPageUrl) {
+function receiveTopItems(items) {
   return {
-    fullName,
-    [CALL_API]: {
-      types: [ STARGAZERS_REQUEST, STARGAZERS_SUCCESS, STARGAZERS_FAILURE ],
-      endpoint: nextPageUrl,
-      schema: Schemas.USER_ARRAY
-    }
-  }
+    type: RECEIVE_TOP_ITEMS,
+    items
+  };
 }
 
-// Fetches a page of stargazers for a particular repo.
-// Bails out if page is cached and user didn’t specifically request next page.
-// Relies on Redux Thunk middleware.
-export function loadStargazers(fullName, nextPage) {
-  return (dispatch, getState) => {
-    const {
-      nextPageUrl = `repos/${fullName}/stargazers`,
-      pageCount = 0
-    } = getState().pagination.stargazersByRepo[fullName] || {}
-
-    if (pageCount > 0 && !nextPage) {
-      return null
-    }
-
-    return dispatch(fetchStargazers(fullName, nextPageUrl))
-  }
+export function fetchTopItems() {
+  return dispatch => {
+    dispatch(requestTopItems());
+    Item.search('findTop10ByOrderByValueDesc')
+      .then(items => dispatch(receiveTopItems(items)));
+  };
 }
 
-export const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE'
+export const REQUEST_ITEM = 'REQUEST_ITEM';
+export const RECEIVE_ITEM = 'RECEIVE_ITEM';
 
-// Resets the currently visible error message.
-export function resetErrorMessage() {
+function requestItem() {
   return {
-    type: RESET_ERROR_MESSAGE
-  }
+    type: REQUEST_ITEM
+  };
+}
+
+function receiveItem(item) {
+  return {
+    type: RECEIVE_ITEM,
+    item
+  };
+}
+
+export function fetchItem(id) {
+  return dispatch => {
+    dispatch(requestItem());
+    Item.findOne(id)
+      .then(item => dispatch(receiveItem(item)));
+  };
 }
--- src/ui/reducers/index.js
+++ src/ui/reducers/index.js
@@ -1,56 +1,69 @@
-import * as ActionTypes from '../actions'
-import merge from 'lodash/merge'
-import paginate from './paginate'
-import { routerReducer as routing } from 'react-router-redux'
-import { combineReducers } from 'redux'
+import * as ActionTypes from '../actions';
+import { routerReducer as routing } from 'react-router-redux';
+import { combineReducers } from 'redux';
 
-// Updates an entity cache in response to any action with response.entities.
-function entities(state = { users: {}, repos: {} }, action) {
-  if (action.response && action.response.entities) {
-    return merge({}, state, action.response.entities)
+function allItems(state = { isFetching: false, items: [] }, action) {
+  switch (action.type) {
+  case ActionTypes.REQUEST_ITEMS:
+    return Object.assign(
+        {},
+        state,
+        { isFetching: true }
+    );
+  case ActionTypes.RECEIVE_ITEMS:
+    return Object.assign(
+        {},
+        state,
+        { isFetching: false, items: action.items }
+    );
+  default:
+    return state;
   }
-
-  return state
 }
 
-// Updates error message to notify about the failed fetches.
-function errorMessage(state = null, action) {
-  const { type, error } = action
-
-  if (type === ActionTypes.RESET_ERROR_MESSAGE) {
-    return null
-  } else if (error) {
-    return action.error
+function topItems(state = { isFetching: false, items: [] }, action) {
+  switch (action.type) {
+  case ActionTypes.REQUEST_TOP_ITEMS:
+    return Object.assign(
+        {},
+        state,
+        { isFetching: true }
+    );
+  case ActionTypes.RECEIVE_TOP_ITEMS:
+    return Object.assign(
+        {},
+        state,
+        { isFetching: false, items: action.items }
+    );
+  default:
+    return state;
   }
-
-  return state
 }
 
-// Updates the pagination data for different actions.
-const pagination = combineReducers({
-  starredByUser: paginate({
-    mapActionToKey: action => action.login,
-    types: [
-      ActionTypes.STARRED_REQUEST,
-      ActionTypes.STARRED_SUCCESS,
-      ActionTypes.STARRED_FAILURE
-    ]
-  }),
-  stargazersByRepo: paginate({
-    mapActionToKey: action => action.fullName,
-    types: [
-      ActionTypes.STARGAZERS_REQUEST,
-      ActionTypes.STARGAZERS_SUCCESS,
-      ActionTypes.STARGAZERS_FAILURE
-    ]
-  })
-})
+function selectedItem(state = { isFetching: false, item: null }, action) {
+  switch (action.type) {
+  case ActionTypes.REQUEST_ITEM:
+    return Object.assign(
+        {},
+        state,
+        { isFetching: true }
+    );
+  case ActionTypes.RECEIVE_ITEM:
+    return Object.assign(
+        {},
+        state,
+        { isFetching: false, item: action.item }
+    );
+  default:
+    return state;
+  }
+}
 
 const rootReducer = combineReducers({
-  entities,
-  pagination,
-  errorMessage,
+  selectedItem,
+  topItems,
+  allItems,
   routing
-})
+});
 
-export default rootReducer
+export default rootReducer;
