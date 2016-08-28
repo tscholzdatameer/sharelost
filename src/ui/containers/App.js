15ec1f24730a85559c092fee0a79e80707ffb7cd
Jörg Weber <weber.joerg.h@gmail.com>
2016-08-28 16:53:17 +0200

    Add progress indictor
    * the progress indictaor appears if the ui needs
      to fetch information from the backend

--- src/ui/containers/App.js
+++ src/ui/containers/App.js
@@ -1,7 +1,10 @@
 import React, { Component, PropTypes } from 'react';
+import connect from 'react-redux/lib/components/connect';
+import filter from 'lodash/filter';
 import Menu from '../components/Menu';
 import AppDrawer from '../components/AppDrawer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LinearProgress from 'material-ui/LinearProgress';
 
 class App extends Component {
   constructor(props) {
@@ -9,11 +12,12 @@ class App extends Component {
   }
 
   render() {
-    const { children } = this.props;
+    const { children, isFetching } = this.props;
     return (
       <MuiThemeProvider>
         <div>
           <Menu />
+          { isFetching ? <LinearProgress mode="indeterminate" max={100} min={0} style={{ position: 'absolute', zIndex: 1101}} color="#3F51B5" /> : null }
           <AppDrawer />
           <div className="container-fluid">
             {children}
@@ -25,9 +29,17 @@ class App extends Component {
 }
 
 App.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  isFetching: PropTypes.bool.isRequired
 };
 
 App.path = '/';
 
-export default App;
+function mapStateToProps(state) {
+  const isFetching = filter(state, 'isFetching');
+  return {
+    isFetching: isFetching.length > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
