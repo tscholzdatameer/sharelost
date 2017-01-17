d5b9383b9b1d1b2de5e3b939d62e7b5ec30a39ad
Sebastian Froehlich <sfroehlich@datameer.com>
2017-01-17 11:06:24 +0100

    #16 added operation to expose Users via Jackson

--- src/main/java/org/sharelost/spark/SparkApplication.java
+++ src/main/java/org/sharelost/spark/SparkApplication.java
@@ -2,6 +2,7 @@ package org.sharelost.spark;
 
 import org.sharelost.spark.persistence.Injector;
 import org.sharelost.spark.persistence.users.UserController;
+import org.sharelost.spark.util.JsonUtil;
 
 import static spark.Spark.get;
 
@@ -12,5 +13,11 @@ public class SparkApplication {
 		get("/hello", (req, res) -> "Hello World");
 
 		get("/users", (req, res) -> UserController.showUsers().toString());
+
+		get("/usersWithJackons", (request, response) -> {
+			response.status(200);
+			response.type("application/json");
+			return JsonUtil.dataToJson(UserController.allUsers().toString());
+		});
 	}
 }
